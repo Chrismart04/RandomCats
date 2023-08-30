01e95d257c5bd485a4ba91d2c82e375b54e1b069
Christopher <chrismartinez.2054@gmail.com>
2023-08-30 13:00:49 -0600

    improved to 3 images modifying the endpoint

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 
 const loadPhoto = async () => {
   try {
-    const URL = "https://api.thecatapi.com/v1/images/search";
+    const URL = "https://api.thecatapi.com/v1/images/search?limit=3";
     const res = await fetch(URL);
     const data = await res.json();
-    const img = document.querySelector('img');
+    const img = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
     img.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
     return photo;
   } catch (err) {
     console.error(err);
